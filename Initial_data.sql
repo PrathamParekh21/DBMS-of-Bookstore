82e75e59ed1542a64ba855c7b4456aaa9b185b09
PrathamParekh21 <160177429+PrathamParekh21@users.noreply.github.com>
2024-06-27 17:03:19 -0400

    Initial_data.sql

---
+++ Initial_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Books (book_id, title, genre, format, author_id, publisher_id, publish_date, price) VALUES
+(1, 'The Great Gatsby', 'Fiction', 'Physical', 1, 1, '1925-04-10', 10.99),
+(2, '1984', 'Dystopian', 'E-Book', 2, 2, '1949-06-08', 8.99);
